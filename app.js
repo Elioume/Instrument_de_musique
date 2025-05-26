e9174e75e570a831b23c209f113c1cb1be10afe9
Elioume <elio.pechillon@gmail.com>
2025-05-26 12:59:24 +0200

    caca demi journée 3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-fetch(`instruments-musique-monde.json`)
+fetch(`instrumentsMusiqueMonde.json`)
 .then(rep=>rep.json())
 .then(data=>{
     console.log(data)
@@ -10,19 +10,70 @@ fetch(`instruments-musique-monde.json`)
     })
 })
 
-function afficherProduits() {
-    document.getElementById(`nosProduitsJS`).innerHTML += ``
-}
+function afficherProduits(produit) {
+    let tabProduits = produit.produits
+    tabProduits.forEach(i=>{
+        document.getElementById(`nosProduitsJS`).innerHTML += `
+            <!-- Placeholder carte produit -->
+                <div class="large-4 carteProduit mb-30">
+                    <!-- Partie haute / Image produit -->
+                    <div>
+                        <img src="${i.imageUrl}" alt="" class="large-12 imgProduit">
+                    </div>
 
-function afficherServices() {
-    document.getElementById(`nosServicesJS`).innerHTML += ``
-}
+                    <!-- Partie basse / Titre + Desc -->
+                    <div>
+                        <h3 class="text-center align-end">${i.nom}</h3>
+                        <p>${i.description}</p>
+                    </div>
 
-function afficherAvis() {
-    document.getElementById(`avisClientJS`).innerHTML += ``
-}
+                    <!-- Bouton -->
+                    <div class="flex justify-end">
+                        <p class="btn">Voir plus</p>
+                    </div>
+                </div>
+`})}
+
+function afficherServices(service) {
+    let tabProduits = service.services
+    tabProduits.forEach(i=>{
+    document.getElementById(`nosServicesJS`).innerHTML += `
+    <!-- Carte placeholder img à gauche, txt à droite -->
+                <div class="flex justify-between container-1000 carteService align-center">
+
+                    <!-- Image Gauche -->
+                    <div class="large-6">
+                        <img src="${i.imageUrl}" alt="" class="large-12 imgService">
+                    </div>
+
+                    <!-- Texte Droite -->
+                    <div class="large-6">
+                        <h2>${i.nom}</h2>
+                        <p>${i.description}</p>
+                    </div>
+                </div>
+`})}
+
+function afficherAvis(temoignage) {
+    let tabProduits = temoignage.temoignages
+    tabProduits.forEach(i=>{
+    document.getElementById(`avisClientJS`).innerHTML += `
+    <!-- Placeholder carte Avis Clients -->
+                <div class="carteClient large-4">
+
+                    <div class="flex justify-between">
+                        <h4>${i.prenom}</h4>
+                        <h4>${i.note}/5</h4>
+                    </div>
+
+                    <h4 class="mt-10">${i.prestation}</h4>
+                    <p>${i.commentaire}</p>
+                </div>
+`})}
 
 // modifier le json pour que les promesses soit un autre tableau avec 1 image et la promesse
-function afficherPromesses() {
-    document.getElementById(`promessesJS`).innerHTML += ``
-}
+function afficherPromesses(promesse) {
+    let tabProduits = promesse.promesses
+    tabProduits.forEach(i=>{
+    document.getElementById(`promessesJS`).innerHTML += `
+`})}
